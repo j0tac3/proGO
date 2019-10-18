2668f4678ebc8a5a45dfde99ce58036797333125
J0tac3 <j0s3.90jr@gmail.com>
2019-10-18 14:49:08 +0200

    Controllers modify

--- ProGoLaravel/app/Http/Controllers/AtaqueCargadoController.php
+++ ProGoLaravel/app/Http/Controllers/AtaqueCargadoController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\AtaqueCargado;
 
 class AtaqueCargadoController extends Controller
 {
-    //
+    public function index(){
+        $ataqueCargado = AtaqueCargado::all();
+        return $ataqueCargado;
+    }
+
+    public function store(Request $request){
+        $ataqueCargado = new AtaqueCargado;
+
+        $ataqueCargado->attackName = $request->attackName;
+        $ataqueCargado->attackType = $request->attackType;
+        $ataqueCargado->danioPVP = $request->danioPVP;
+        $ataqueCargado->danioNormal = $request->danioNormal;
+        $ataqueCargado->speed = $request->speed;
+        $ataqueCargado->save;
+    }
+
+    public function update(Request $request){
+        $ataqueCargado = new AtaqueCargado;
+
+        $ataqueCargado->attackName = $request->attackName;
+        $ataqueCargado->attackType = $request->attackType;
+        $ataqueCargado->danioPVP = $request->danioPVP;
+        $ataqueCargado->danioNormal = $request->danioNormal;
+        $ataqueCargado->speed = $request->speed;
+        $ataqueCargado->save;
+    }
+
+    public function destroy(Request $request){
+        $ataqueCargado = new AtaqueCargado;
+
+        $ataqueCargado->delete($request->loadAttackID);
+    }
 }
--- ProGoLaravel/app/Http/Controllers/PokemonController.php
+++ ProGoLaravel/app/Http/Controllers/PokemonController.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pokemon;
 
 class PokemonController extends Controller
 {
-    //
+    public function index(){
+        $pokemon = Pokemon::all();
+        return $pokemon;
+    }
+
+    public function store(Request $request){
+        $pokemon = new Pokemon;
+
+        $pokemon->pokedex = $request->pokedex;
+        $pokemon->name = $request->name;
+        $pokemon->description = $request->description;
+        $pokemon->tipo = $request->tipo;
+        $pokemon->height = $request->height;
+        $pokemon->weight = $request->weight;
+        $pokemon->sex = $request->sex;
+        $pokemon->preEvolution = $request->preEvolution;
+        $pokemon->evolution = $request->evolution;
+        $pokemon->region = $request->region;
+        $pokemon->image = $request->image;
+        $pokemon->activeGo = $request->activeGo;
+        $pokemon->activeGoShiny = $request->activeGoShiny;
+        $pokemon->save;
+    }
+
+    public function update(Request $request){
+        $pokemon = new Pokemon;
+
+        $pokemon->pokedex = $request->pokedex;
+        $pokemon->name = $request->name;
+        $pokemon->description = $request->description;
+        $pokemon->tipo = $request->tipo;
+        $pokemon->height = $request->height;
+        $pokemon->weight = $request->weight;
+        $pokemon->sex = $request->sex;
+        $pokemon->preEvolution = $request->preEvolution;
+        $pokemon->evolution = $request->evolution;
+        $pokemon->region = $request->region;
+        $pokemon->image = $request->image;
+        $pokemon->activeGo = $request->activeGo;
+        $pokemon->activeGoShiny = $request->activeGoShiny;
+        $pokemon->save;
+    }
+
+    public function destroy(Request $request){
+        $pokemon = new Pokemon;
+
+        $pokemon->delete($pokemon->regionID);
+    }
 }
