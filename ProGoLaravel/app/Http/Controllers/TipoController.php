627976b07a1b5b1d690ab4c4abefd150b5d1232a
J0tac3 <j0s3.90jr@gmail.com>
2019-10-23 20:26:47 +0200

    Controllers modify

--- ProGoLaravel/app/Http/Controllers/TipoController.php
+++ ProGoLaravel/app/Http/Controllers/TipoController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tipo;
 
 class TipoController extends Controller
 {
-    //
+    public function index(){
+        $tipo = Tipo::all();
+        return $tipo;
+    }
+
+    public function store(Request $request){
+        $tipo = new Tipo;
+
+        $tipo->typeName = $request->typeName;
+        $tipo->save;
+    }
+
+    public function update(Request $request){
+        $tipo = new Tipo;
+
+        $tipo->typeName = $request->typeName;
+        $tipo->save;
+    }
+
+    public function destroy(Request $request){
+        $tipo = new Tipo;
+
+        $tipo->delete($pokemon->typeID);
+    }
 }
